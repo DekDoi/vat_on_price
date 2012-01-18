25d4c2ee74e7b483263e40e32bf7ae5f7a90f647
Dek Doi <9dekdoi@gmail.com>
2012-01-18 17:15:10 +0700

    modified rspec test

--- spec/price_vat_three_spec.rb
+++ spec/price_vat_three_spec.rb
@@ -12,16 +12,26 @@ describe PriceVatThree do
     end
   end
   
-  describe "#before_vat" do
-    it "returns a price before VAT that equals to 794.3925233644859 " do
-      @price_vat.before_vat(850.0).should eq 794.3925233644859 
-    end
-  end
+  describe "#method_missing" do
   
-  describe "#after_vat" do
-    it "returns a price after VAT that equals to 850" do
-      @price_vat.after_vat(794.3925233644859).should eq 850.0 
+    describe "method's name is 'before_vat'" do
+      it "returns a price before VAT that equals to 794.3925233644859 " do
+        @price_vat.before_vat(850.0).should eq 794.3925233644859 
+      end
     end
+    
+    describe "method's name is 'after_vat'" do
+      it "returns a price after VAT that equals to 850" do
+        @price_vat.after_vat(794.3925233644859).should eq 850.0 
+      end
+    end
+    
+    describe "method's name doesn't match any conditions or incorrect format" do
+      it "returns 0" do
+        @price_vat.discount_price(794.3925233644859).should eq 0
+      end
+    end
+    
   end
   
   describe "#vat_price" do
